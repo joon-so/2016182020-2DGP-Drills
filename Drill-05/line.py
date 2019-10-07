23e75b1b2c89246aae5216ccf759f7db79737e4b
joon-so(notebook) <joonso16@gmail.com>
2019-10-08 02:06:31 +0900

    drill-05 최종
    최종본

--- Drill-05/line.py
+++ Drill-05/line.py
@@ -1,33 +1,28 @@
-import random
 from pico2d import *
-
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
-
-def draw_charecter(p1, p2):
-    global direct
-    global frame
-    if direct == 0:
-        character.clip_draw(frame * 100, 100, 100, 100, p1, p2)
-        update_canvas()
-        frame = (frame + 1) % 8
-    elif direct == 1:
-        character.clip_draw(frame * 100, 0, 100, 100, p1, p2)
-        update_canvas()
-        frame = (frame + 1) % 8
-
-    pass
 
 
 def move_charecter(p1, p2):
-    global frame
-    global character_x, character_y
+    global frame, direct
     global t
     global i
-    i += 10
-    t = i / 100
-    character_x = (1 - t) * p1 + t * p2
-    character_y = (1 - t) * p1 + t * p2
-
+    global character_x, character_y, move_x, move_y, x, y
+    if i != 100:
+        i += 10
+    elif i == 100:
+        character_x = move_x
+        character_y = move_y
+    t = 1 / 100 * i
+    x = (1 - t) * p1[0] + t * p2[0]
+    y = (1 - t) * p1[1] + t * p2[1]
+    if direct == 0:
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+    elif direct == 1:
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
     pass
 
 
@@ -35,7 +30,7 @@ def handle_events():
     global running
     global mouse_x, mouse_y
     global move_x, move_y
-    global character_x, character_y
+    global character_x, character_y, x, y
     global direct
     global t, i
     events = get_events()
@@ -49,6 +44,8 @@ def handle_events():
         elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
             move_x = mouse_x
             move_y = mouse_y
+            character_x = x
+            character_y = y
             t = 0
             i = 0
             if character_x > move_x:
@@ -71,24 +68,16 @@ direct = 0
 frame = 0
 t = 0
 i = 0
+x, y = 500, 500
+
 hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    Mouse.draw_now(mouse_x + 30, mouse_y - 30)
+    Mouse.draw_now(mouse_x + 25, mouse_y - 30)
 
-    #move_charecter(move_x, move_y)
-
-    if direct == 0:
-        character.clip_draw(frame * 100, 100, 100, 100, move_x, move_y)
-        update_canvas()
-        frame = (frame + 1) % 8
-    elif direct == 1:
-        character.clip_draw(frame * 100, 0, 100, 100, move_x, move_y)
-        update_canvas()
-        frame = (frame + 1) % 8
-
+    move_charecter((character_x, character_y), (move_x, move_y))
 
     update_canvas()
     delay(0.03)
