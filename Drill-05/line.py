5cc942d79c788a191fe31b77a5d88b5283533b14
joon-so(notebook) <joonso16@gmail.com>
2019-10-07 20:06:01 +0900

    drill05 캐릭터 이동

--- Drill-05/line.py
+++ Drill-05/line.py
@@ -4,13 +4,29 @@ from pico2d import *
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 def draw_charecter(p1, p2):
-    global character_x, character_y
     global direct
     global frame
-    for i in range(0, 100 + 1, 2):
-        t = i / 100
-        character_x = (1 - t) * p1[0] + t * p2[0]
-        character_y = (1 - t) * p1[1] + t * p2[1]
+    if direct == 0:
+        character.clip_draw(frame * 100, 100, 100, 100, p1, p2)
+        update_canvas()
+        frame = (frame + 1) % 8
+    elif direct == 1:
+        character.clip_draw(frame * 100, 0, 100, 100, p1, p2)
+        update_canvas()
+        frame = (frame + 1) % 8
+
+    pass
+
+
+def move_charecter(p1, p2):
+    global frame
+    global character_x, character_y
+    global t
+    global i
+    i += 10
+    t = i / 100
+    character_x = (1 - t) * p1 + t * p2
+    character_y = (1 - t) * p1 + t * p2
 
     pass
 
@@ -21,6 +37,7 @@ def handle_events():
     global move_x, move_y
     global character_x, character_y
     global direct
+    global t, i
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -32,6 +49,8 @@ def handle_events():
         elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
             move_x = mouse_x
             move_y = mouse_y
+            t = 0
+            i = 0
             if character_x > move_x:
                 direct = 1
             else:
@@ -50,13 +69,17 @@ move_x, move_y = 500, 500
 character_x, character_y = 500, 500
 direct = 0
 frame = 0
-
+t = 0
+i = 0
 hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     Mouse.draw_now(mouse_x + 30, mouse_y - 30)
+
+    #move_charecter(move_x, move_y)
+
     if direct == 0:
         character.clip_draw(frame * 100, 100, 100, 100, move_x, move_y)
         update_canvas()
@@ -65,11 +88,9 @@ while running:
         character.clip_draw(frame * 100, 0, 100, 100, move_x, move_y)
         update_canvas()
         frame = (frame + 1) % 8
-   # draw_charecter(move_x, move_y)
 
-    delay(0.03)
     update_canvas()
-
+    delay(0.03)
     handle_events()
 
 close_canvas()
