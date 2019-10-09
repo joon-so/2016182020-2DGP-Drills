9fa2e1fcfa74fe1d73c0ebc628f2bafef7cb3c1e
joon-so(notebook) <joonso16@gmail.com>
2019-10-09 18:11:47 +0900

    drill-06_2 움직임 함수
    움직임 함수 만듬

--- Drill-06_2/8_2.py
+++ Drill-06_2/8_2.py
@@ -28,34 +28,38 @@ def move_charecter(p1, p2):
         frame = (frame + 1) % 8
     pass
 
+
 def draw_curve_4_points(p1, p2, p3, p4):
     # draw p1-p2
     global t, i
-    t = i / 100
-    x = (2 * t ** 2 - 3 * t + 1) * p1[0] + (-4 * t ** 2 + 4 * t) * p2[0] + (2 * t ** 2 - t) * p3[0]
-    y = (2 * t ** 2 - 3 * t + 1) * p1[1] + (-4 * t ** 2 + 4 * t) * p2[1] + (2 * t ** 2 - t) * p3[1]
+    global to
+    global ch_i
+    global frame
 
+    for i in range(0, 50, 10):
+        t = i / 100
+        x = (2 * t ** 2 - 3 * t + 1) * p1[0] + (-4 * t ** 2 + 4 * t) * p2[0] + (2 * t ** 2 - t) * p3[0]
+        y = (2 * t ** 2 - 3 * t + 1) * p1[1] + (-4 * t ** 2 + 4 * t) * p2[1] + (2 * t ** 2 - t) * p3[1]
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
 
     # draw p2-p3
-    t = i / 100
-    x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (-3 * t ** 3 + 4 * t ** 2 + t) * p3[0] +
-    (t ** 3 - t ** 2) * p4[0]) / 2
-    y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (-3 * t ** 3 + 4 * t ** 2 + t) * p3[1] +
-    (t ** 3 - t ** 2) * p4[1]) / 2
+    for i in range(0, 100, 10):
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] +
+             (t ** 3 - t ** 2) * p4[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] +
+             (t ** 3 - t ** 2) * p4[1]) / 2
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
 
-    # draw p3-p4
-    t = i / 100
-    x = (2 * t ** 2 - 3 * t + 1) * p2[0] + (-4 * t ** 2 + 4 * t) * p3[0] + (2 * t ** 2 - t) * p4[0]
-    y = (2 * t ** 2 - 3 * t + 1) * p2[1] + (-4 * t ** 2 + 4 * t) * p3[1] + (2 * t ** 2 - t) * p4[1]
+    ch_i += 1
 
-
-    # draw p4-p1
-    t = i / 100
-    x = ((-t ** 3 + 2 * t ** 2 - t) * p3[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[0] + (-3 * t ** 3 + 4 * t ** 2 + t) * p1[0] +
-    (t ** 3 - t ** 2) * p2[0]) / 2
-    y = ((-t ** 3 + 2 * t ** 2 - t) * p3[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[1] + (-3 * t ** 3 + 4 * t ** 2 + t) * p1[1] +
-    (t ** 3 - t ** 2) * p2[1]) / 2
-
+    pass
 
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
@@ -67,7 +71,13 @@ running = True
 character_x, character_y = 500, 500
 t = 0
 i = 0
+ch_i = 0
+to = 0
+frame = 0
 x, y = 500, 500
+
+ch_x = [(random.randint(-500, 500), random.randint(-500, 500)) for i in 10]
+ch_y = [(random.randint(-500, 500), random.randint(-500, 500)) for i in 10]
 
 hide_cursor()
 
