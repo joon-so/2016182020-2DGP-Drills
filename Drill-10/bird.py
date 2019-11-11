0f11757175a43d230e49b81d5809168d7fe4b949
joon-so(notebook) <joonso16@gmail.com>
2019-11-11 18:46:28 +0900

    Drill-10 새 좌우 구현
    2016182020 소준 commit : 3

--- Drill-10/bird.py
+++ Drill-10/bird.py
@@ -16,15 +16,35 @@ TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 8
 
+FLY = range(1)
+
+class IdleState:
+
+    @staticmethod
+    def enter(bird, event):
+        pass
+
+    @staticmethod
+    def exit(bird, event):
+        pass
+
+    @staticmethod
+    def do(bird):
+        bird.add_event(FLY)
+
+    @staticmethod
+    def draw(bird):
+        pass
+
 
 class RunState:
     @staticmethod
     def enter(bird, event):
-        if bird.dir == 1:
+        if bird.direct == 1:
             bird.velocity += RUN_SPEED_PPS
-        elif bird.dir == -1:
+        elif bird.direct == -1:
             bird.velocity -= RUN_SPEED_PPS
-        bird.dir = clamp(-1, bird.velocity, 1)
+        #bird.dir = clamp(-1, bird.velocity, 1)
         pass
 
     @staticmethod
@@ -35,11 +55,17 @@ class RunState:
     def do(bird):
         bird.frame = (bird.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 5
         bird.x += bird.velocity * game_framework.frame_time
-        bird.x = clamp(25, bird.x, 1600 - 25)
+        bird.x = clamp(75, bird.x, 1600 - 75)
+        if bird.x == 1600 - 75:
+            bird.direct = -1
+        elif bird.x == 75:
+            bird.direct = 1
+
+
 
     @staticmethod
     def draw(bird):
-        if bird.dir == 1:
+        if bird.direct == 1:
             if bird.frame_y == 0:
                 if int(bird.frame) == 4:
                     bird.frame = 0
@@ -48,24 +74,40 @@ class RunState:
             if int(bird.frame) == 4:
                 bird.frame_y -= 1
         else:
-            bird.image.clip_draw(int(bird.frame) * 182, 338, 182, 169, bird.x, bird.y)
+            bird.image.clip_composite_draw(int(bird.frame) * 182, bird.frame_y * 168, 182, 168, 3.141592 / 1, 'v', bird.x,
+                                          bird.y, 182, 168)
+
+next_state_table = {
+    IdleState: {FLY: RunState}
+}
 
 
 class Bird:
 
     def __init__(self):
-        self.x, self.y = 1600 // 2, 90
+        self.x, self.y = 1600 // 2, 400
         # Boy is only once created, so instance image loading is fine
         self.image = load_image('bird_animation.png')
         self.font = load_font('ENCR10B.TTF', 16)
-        self.dir = 1
+        self.direct = 1
         self.velocity = 0
-        self.cur_state = RunState
+        self.cur_state = IdleState
+        self.event_que = []
+        self.cur_state = IdleState
+        self.cur_state.enter(self, None)
         self.frame = 0
         self.frame_y = 2
 
+    def add_event(self, event):
+        self.event_que.insert(0, event)
+
     def update(self):
         self.cur_state.do(self)
+        if len(self.event_que) > 0:
+            event = self.event_que.pop()
+            self.cur_state.exit(self, event)
+            self.cur_state = next_state_table[self.cur_state][event]
+            self.cur_state.enter(self, event)
 
     def draw(self):
         self.cur_state.draw(self)
